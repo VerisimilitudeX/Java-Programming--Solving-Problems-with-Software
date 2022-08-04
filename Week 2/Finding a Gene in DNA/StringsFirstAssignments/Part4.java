c1d6c7aa627c4221a0ccf5e978b25a81cdeae98d
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-03 17:49:18 -0700

    Created the Part4 class.

---
+++ Week 2/Finding a Gene in DNA/StringsFirstAssignments/Part4.java
@@ -0,0 +1,8 @@
+import edu.duke.URLResource;
+
+public class Part4 {
+    URLResource resource = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+    for (String word : resource.words()) {
+        
+    }
+}
