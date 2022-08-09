d54a6af2660eef8cc7337713315e8450c283b45f
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-09 00:21:58 -0700

    Commented out some lines used for debugging.

--- Week 2/Debugging Code/Main1.java
+++ Week 2/Debugging Code/Main1.java
@@ -1,19 +1,18 @@
-public class debuggingPart1 {
+public class Main1 {
     public static void findAbc(String input) {
         int index = input.indexOf("abc");
         while (true) {
-            if (index == -1) {
+            if (index == -1 || index >= input.length() - 3) {
                 break;
             }
+            // System.out.println(index+1);
+            // System.out.println(index+4);
             String found = input.substring(index+1, index+4);
             System.out.println(found);
             index = input.indexOf("abc", index+4);
         }
     }
-    public static void test() {
-        findAbc("abcdabc");    
-    }
     public static void main(String[] args) {
-    test();
+        findAbc("abcdabc");    
     }
 }
