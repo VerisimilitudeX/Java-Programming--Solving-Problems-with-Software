5abc5cf7edd9560f58a410ff52e2065e72481522
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-09 00:56:27 -0700

    Added another test case.

--- Week 2/Debugging Code/debuggingPart2.java
+++ Week 2/Debugging Code/debuggingPart2.java
@@ -5,14 +5,12 @@ public class debuggingPart2 {
             if (index == -1 || index >= input.length() - 3){
                 break;
             }
-            System.out.println("index " + index);
             String found = input.substring(index+1, index+4);
             System.out.println(found);
-            index = input.indexOf("abc",index+4);
-            System.out.println("index after updating " + index);
+            index = input.indexOf("abc",index+3);
         }
     }
     public static void main(String[] args){
-        findAbc("abcdkfjsksioehgjfhsdjfhksdfhuwabcabcajfieowj");
+        findAbc("abcabcabcabca");
     }
 }
