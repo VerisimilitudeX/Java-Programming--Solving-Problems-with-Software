d4fb5ef5e928aaecc2c4db2ad403af303607f8a2
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-09 00:33:01 -0700

    Added print statements to debug code

--- Week 2/Debugging Code/debuggingPart2.java
+++ Week 2/Debugging Code/debuggingPart2.java
@@ -5,12 +5,13 @@ public class debuggingPart2 {
             if (index == -1 || index >= input.length() - 3){
                 break;
             }
+            System.out.println("index " + index);
             String found = input.substring(index+1, index+4);
             System.out.println(found);
             index = input.indexOf("abc",index+4);
+            System.out.println("index after updating " + index);
         }
     }
- 
     public static void main(String[] args){
         findAbc("abcdkfjsksioehgjfhsdjfhksdfhuwabcabcajfieowj");
     }
