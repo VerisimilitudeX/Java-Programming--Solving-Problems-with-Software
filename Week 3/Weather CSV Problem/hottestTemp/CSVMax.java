5a940fa477302d5aecb61c3c7c553403b0e7b948
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-12 13:49:00 -0700

    Refactored code.

--- Week 3/Weather CSV Problem/hottestTemp/CSVMax.java
+++ Week 3/Weather CSV Problem/hottestTemp/CSVMax.java
@@ -2,26 +2,26 @@ import duke.*;
 import csv.*;
 public class CSVMax {
 	public static CSVRecord hottestHourInFile(CSVParser parser) {
-		//start with largestSoFar as nothing
+		// Start with largestSoFar as nothing
 		CSVRecord largestSoFar = null;
-		//For each row (currentRow) in the CSV File
+		// For each row (currentRow) in the CSV File
 		for (CSVRecord currentRow : parser) {
-			//If largestSoFar is nothing
+			// If largestSoFar is nothing
 			if (largestSoFar == null) {
 				largestSoFar = currentRow;
 			}
-			//Otherwise
+			// Otherwise
 			else {
 				double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
 				double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
-				//Check if currentRow’s temperature > largestSoFar’s
+				// Check if currentRow’s temperature > largestSoFar’s
 				if (currentTemp > largestTemp) {
-					//If so update largestSoFar to currentRow
+					// If so update largestSoFar to currentRow
 					largestSoFar = currentRow;
 				}
 			}
 		}
-		//The largestSoFar is the answer
+		// The largestSoFar is the answer
 		return largestSoFar;
 	}
 
